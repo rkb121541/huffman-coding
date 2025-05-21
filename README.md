ce10620648f2c60d5c29346c26a8bb7f79629be0
rkb121541 <rkbidarkota@gmail.com>
2025-05-21 16:06:25 -0400

    finished encodings

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Huffman Coding
 
 In my implementation, I order the priority queue from min to max
+
+Since I use ```std::format()``` which is only available in C++ versions 20 and above, I have to compile ```main.cpp``` by running ```g++ -std=c++20 main.cpp```
--- main.cpp
+++ main.cpp
@@ -10,9 +10,10 @@ public:
   int frequency;
   Node *left = nullptr;
   Node *right = nullptr;
+  std::string encoding = "";
 };
 
-void sortQueue(Node *node, std::deque<Node*> *priorityQueue) {
+void addToQueue(Node *node, std::deque<Node*> *priorityQueue) {
   bool added = false; // to see if node was already added
   for (std::deque<Node*>::iterator it = priorityQueue->begin();
        it != priorityQueue->end(); ++it) { // it is type iterator (basically like a ptr) / Node**
@@ -27,13 +28,17 @@ void sortQueue(Node *node, std::deque<Node*> *priorityQueue) {
   }
 }
 
-void printTree(Node* root) {
-  std::cout << std::format("({} {})", root->name, root->frequency) << std::endl;
+// initially used to print tree nodes; now i use to make the encodings
+void traverseTree(Node* root, std::map<std::string, std::string>* encodings) { // makes sure that the tree is being made properly
   if (root->left == nullptr && root->right == nullptr) {
+    // std::cout << std::format("({} {})", root->name, root->encoding) << std::endl;
+    (*encodings)[root->name] = root->encoding; 
     return;
   }
-  printTree(root->left);
-  printTree(root->right);
+  root->left->encoding.append(root->encoding).append("0");
+  traverseTree(root->left, encodings);
+  root->right->encoding.append(root->encoding).append("1");
+  traverseTree(root->right, encodings);
 }
 
 int main(int argc, char const *argv[]) {
@@ -62,8 +67,14 @@ int main(int argc, char const *argv[]) {
     node->frequency = firstSmallest->frequency + secondSmallest->frequency;
     node->left = firstSmallest;
     node->right = secondSmallest;
-    sortQueue(node, &priorityQueue); // FUNCTION TO PUT IT IN THE RIGHT SPOT
+    addToQueue(node, &priorityQueue); // FUNCTION TO PUT IT IN THE RIGHT SPOT
   }
-  printTree(priorityQueue.front());
+  std::map<std::string, std::string> encodings;
+  traverseTree(priorityQueue.front(), &encodings);
+  for (const auto& pair : encodings) { // printing out encodings map
+    std::cout << std::format("{}:{}, ", pair.first, pair.second);
+  }
+  std::cout << std::endl;
+
   return 0;
 }
